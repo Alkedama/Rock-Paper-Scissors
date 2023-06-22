ba4191c42f82280e845fc75c2f2a2b056bab1c46
King-Windows <krsazon@gmail.com>
2023-06-21 23:44:57 -0700

    Updates RockPaperScissors

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 let getComputerChoice = () => {
     let min = 1;
     let max = 3;
-    let random = Math.floor(Math.random() * (max - min + 1) + min);
+    let mathRandom = Math.random();
+    let random = Math.floor(mathRandom * (max - min + 1) + min);
 
     if (random === 1) {
         return "rock";
@@ -14,24 +15,86 @@ let getComputerChoice = () => {
     }
 }
 
-let playerSelection = () => {
-    
+function playerSelection(){
+    let answer = prompt("Choose Rock, Paper or Scissors");
+    let trueAnswer = answer.charAt(0).toUpperCase() + answer.slice(1).toLowerCase();
+
+    return trueAnswer;
 }
 
 let computerSelection = () => {
+    let min = 1;
+    let max = 3;
+    let mathRandom = Math.random();
+    let random = Math.floor(mathRandom * (max - min + 1) + min);
 
+    if (random === 1) {
+        return "Rock";
+    } else if (random === 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
 }
 
 let playRound = (playerSelection, computerSelection) => {
-    
+
+    if (playerSelection == "Rock" && computerSelection == "Rock") {
+        console.log(`You play ${playerSelection}`);
+        console.log(`Opponent plays ${computerSelection}`);
+        console.log("Tie");
+    } else if (playerSelection == "Rock" && computerSelection == "Paper") {
+        console.log(`You play ${playerSelection}`);
+        console.log(`Opponent plays ${computerSelection}`);
+        console.log("You lose");
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        console.log(`You play ${playerSelection}`);
+        console.log(`Opponent plays ${computerSelection}`);
+        console.log("You win");
+    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        console.log(`You play ${playerSelection}`);
+        console.log(`Opponent plays ${computerSelection}`);
+        console.log("You win");
+    } else if (playerSelection == "Paper" && computerSelection == "Paper") {
+        console.log(`You play ${playerSelection}`);
+        console.log(`Opponent plays ${computerSelection}`);
+        console.log("Tie");
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        console.log(`You play ${playerSelection}`);
+        console.log(`Opponent plays ${computerSelection}`);
+        console.log("You lose");
+    } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        console.log(`You play ${playerSelection}`);
+        console.log(`Opponent plays ${computerSelection}`);
+        console.log("You lose");
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        console.log(`You play ${playerSelection}`);
+        console.log(`Opponent plays ${computerSelection}`);
+        console.log("You win");
+    } else if (playerSelection == "Scissors" && computerSelection == "Scissors") {
+        console.log(`You play ${playerSelection}`);
+        console.log(`Opponent plays ${computerSelection}`);
+        console.log("Tie");
+    } else {
+        console.log(`You play ${playerSelection}`);
+        console.log(`Opponent plays ${computerSelection}`);
+        console.log("Invalid Input. Play again.");
+        playRound();
+       
+    }
+
+   
+
 }
+
+playRound(playerSelection(), computerSelection());
 
 // console.log(playRound("rock", getComputerChoice()));
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
 let game = () => {
-    prompt("Enter rock, paper, or scissors");
+    playerSelection();
 }
 
 // game();
